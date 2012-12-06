7bf197331ea704bfbd698aa25d3859eab67fea4d
Austin King <shout@ozten.com>
2012-12-06 14:33:38 -0800

    primary_shuts_down_signle_email

---
+++ automation-tests/tests/idp_transition/primary_shuts_down_signle_email.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const
+path = require('path'),
+assert = require('../../lib/asserts.js'),
+restmail = require('../../lib/restmail.js'),
+utils = require('../../lib/utils.js'),
+persona_urls = require('../../lib/urls.js'),
+CSS = require('../../pages/css.js'),
+dialog = require('../../pages/dialog.js'),
+runner = require('../../lib/runner.js'),
+testSetup = require('../../lib/test-setup.js');
+
+var browser, testUser, testIdp;
+
+runner.run(module, {
+  "setup": function(done) {
+    testSetup.setup({browsers: 1, testidps: 1}, function(err, fixtures) {
+      if (fixtures) {
+        browser = fixtures.browsers[0];
+        testUser = fixtures.testidps[0].email;
+        // testIdp {domain, password}
+        testIdp = fixtures.testidps[0].idp;
+      }
+      done(err);
+    });
+  },
+  "create a new selenium session": function(done) {
+    testSetup.newBrowserSession(browser, done);
+  },
+  "load 123done and wait for the signin button to be visible": function(done) {
+    browser.get(persona_urls["123done"], done);
+  },
+  "click the signin button": function(done, el) {
+    browser.wclick(CSS['123done.org'].signinButton, done);
+  },
+  "switch to the dialog when it opens": function(done) {
+    browser.wwin(CSS["persona.org"].windowName, done);
+  }
+},
+{
+  suiteName: path.basename(__filename),
+  cleanup: function(done) { testSetup.teardown(done) }
+});
