92c512402955fc73a985c69614f64e93b9af898c
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-12-06 12:44:51 -0700

    sketch flow of secondary -> primary transition test in more detail

--- automation-tests/tests/idp_transition/single_email_primary_starts_up.js
+++ automation-tests/tests/idp_transition/single_email_primary_starts_up.js
@@ -13,12 +13,16 @@ persona_urls = require('../../lib/urls.js'),
 CSS = require('../../pages/css.js'),
 dialog = require('../../pages/dialog.js'),
 runner = require('../../lib/runner.js'),
+userMaker = require('../../lib/user_maker.js'),
 testSetup = require('../../lib/test-setup.js');
 
 var browser;
 
+var email1, email2;
+
 runner.run(module, {
   "setup": function(done) {
+    // XXX: allocate a testidp domain here
     testSetup.setup({browsers: 1}, function(err, fixtures) {
       if (fixtures) {
         browser = fixtures.browsers[0];
@@ -26,19 +30,46 @@ runner.run(module, {
       done(err);
     });
   },
+  "disable idp so we can create secondary users": function(done) {
+    // XXX
+    done();
+  },
+  "create two secondary users using the domain": function(done) {
+    email1 = 'test1@doesnotexist.testidp.org';
+    email2 = 'test2@doesnotexist.testidp.org';
+
+    userMaker({
+      email: email1,
+      password: 'password'
+    }, function(err) {
+      if (err) return done(err);
+      userMaker({
+        email: email2,
+        password: 'password'
+      }, done);
+    });
+
+  },
   "create a new selenium session": function(done) {
     testSetup.newBrowserSession(browser, done);
   },
-  "load 123done and wait for the signin button to be visible": function(done) {
-    browser.get(persona_urls["123done"], done);
+  "enable primary support on our domain": function(done, el) {
+    // XXX
+    done();
   },
-  "click the signin button": function(done, el) {
-    browser.wclick(CSS['123done.org'].signinButton, done);
+  "load 123done, spawn dialog, switch to it": function(done) {
+    browser.chain()
+      .get(persona_urls["123done"])
+      .wclick(CSS['123done.org'].signinButton, done)
+      .wwin(CSS["persona.org"].windowName, done);
   },
-  "switch to the dialog when it opens": function(done) {
-    browser.wwin(CSS["persona.org"].windowName, done);
+  "enter previously secondary email address": function(done) {
+    broswer.chain()
+      .wtype(CSS['dialog'].emailInput, opts.email)
+      .wclick(CSS['dialog'].newEmailNextButton);
+    // XXX: now we should be transitioned to the "this site
+    // is now a primary" screen
   },
-  // XXX
 },
 {
   suiteName: path.basename(__filename),
